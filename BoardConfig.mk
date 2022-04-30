ec1616c89922bc60216af92c349312f36ce2e662
Wheels564 <53055870+Wheels564@users.noreply.github.com>
2022-04-30 16:14:56 -0400

    Update BoardConfig.mk
    update selinux to system ext

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,8 +29,8 @@ VENDOR_SECURITY_PATCH := 2020-06-04
 
 # SELinux
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # Inherit from the proprietary version
 -include vendor/motorola/james/BoardConfigVendor.mk
