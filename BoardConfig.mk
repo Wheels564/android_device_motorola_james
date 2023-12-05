548b02f4cdb3aac79a4309998ecb679ff5b187fe
Wheels564 <53055870+Wheels564@users.noreply.github.com>
2023-12-05 15:17:13 -0500

    Update BoardConfig.mk
    bring up from hannah

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,33 +4,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from msm8937-common
--include device/motorola/msm8937-common/BoardConfigCommon.mk
+# Inherit from hannah-common
+-include device/motorola/hannah-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/motorola/james
 
-# Display
-TARGET_SCREEN_HEIGHT := 1280
-TARGET_SCREEN_WIDTH := 720
-TARGET_SCREEN_DENSITY := 320
-
-# HIDL
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+# Assert
+TARGET_OTA_ASSERT_DEVICE := james
 
 # Kernel
-TARGET_KERNEL_CONFIG := james_defconfig
-
-# Partitions
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16879616
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 11490278400
-
-# Security patch level
-VENDOR_SECURITY_PATCH := 2020-06-04
-
-# SELinux
-BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
-
-# Inherit from the proprietary version
--include vendor/motorola/james/BoardConfigVendor.mk
+TARGET_KERNEL_CONFIG := james_int_defconfig
+TARGET_KERNEL_RECOVERY_CONFIG := james_recovery_defconfig
