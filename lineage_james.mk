db9e42c3ca43a40e93d1c29cd063bfba95c64825
Wheels564 <53055870+Wheels564@users.noreply.github.com>
2026-02-05 13:51:25 -0500

    Inherit common PARTNER_GMS configurations
    Added inclusion of common PARTNER_GMS configurations.

--- lineage_james.mk
+++ lineage_james.mk
@@ -13,6 +13,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Inherit some common PARTNER_GMS stuff.
+$(call inherit-product, vendor/partner_gms/products/gms.mk)
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_james
 PRODUCT_DEVICE := james
