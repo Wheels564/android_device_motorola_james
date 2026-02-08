3e213d9c25029726c0fa60bde15e45cc97a2eaec
Wheels564 <53055870+Wheels564@users.noreply.github.com>
2026-02-08 15:21:04 -0500

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,6 +11,8 @@ DEVICE_PATH := device/motorola/james
 
 ALLOW_MISSING_DEPENDENCIES=true
 
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+
 # Assert
 TARGET_OTA_ASSERT_DEVICE := james,rjames
 
